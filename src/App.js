fab9e887c95f6942d91b714e15965236726ccec1
josebasilioo <josebasiliosilvaneto@gmail.com>
2023-12-08 11:04:36 -0300

    main: changed main page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function App() {
         <p>
           Mudanças!
         </p>
+        <p>
+          Outra mudança!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
