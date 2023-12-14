eaab60674c1e25256dd719d18dfa61d74edb3680
josebasilioo <josebasiliosilvaneto@gmail.com>
2023-12-14 12:43:09 -0300

    main: added script to start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,12 +3,11 @@ nomeApp="helloReact"
 cd /appDeploy
 npm install
 
-
-echo "getting process"
 process=`sudo lsof -i :9001`
 pid=`echo $process | grep -oP '\b\d+\b' | head -n 1`
 sudo kill -9 $pid
 
 sudo supervisord
 sudo supervisorctl start $nomeApp
+npm start
 
