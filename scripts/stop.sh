8166e2835f752f82d0830387af328e42ca7752bb
josebasilioo <josebasiliosilvaneto@gmail.com>
2023-12-14 12:36:53 -0300

    main: changed only start.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 nomeApp="helloReact"
 
-process=`sudo lsof -i :9001`
-
-if [ -z "${process}" ]; then
-    echo "Empty process... starting"
-else
-    echo "Stopping supervisor app..."
-    sudo supervisorctl stop $nomeApp
-fi
+sudo supervisorctl stop $nomeApp
