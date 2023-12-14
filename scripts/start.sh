49d1d11ca48bfd52f1566543f6c73f661159d52c
josebasilioo <josebasiliosilvaneto@gmail.com>
2023-12-14 12:08:17 -0300

    main: killing process at start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,16 @@ nomeApp="helloReact"
 cd /appDeploy
 npm install
 
+process=`sudo lsof -i :9001`
+pid=`echo $process | grep -oP '\b\d+\b' | head -n 1`
+
+if [ -z "${pid}" ]; then
+    echo "Empty process... starting"
+else
+    echo "Killing process..."
+    sudo kill -9 $pid
+fi
+
 sudo supervisord
 sudo supervisorctl start $nomeApp
 npm start
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 nomeApp="helloReact"
 
-process=`sudo lsof -i :9001`
-pid=`echo $process | grep -oP '\b\d+\b' | head -n 1`
-
 sudo supervisorctl stop $nomeApp
-
-if [ -z "${pid}" ]; then
-    echo "Empty process... starting"
-else
-    echo "Killing process..."
-    sudo kill -9 $pid
-fi
