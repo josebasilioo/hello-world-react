0069c05e0b8d5f5d2161bfe64bb4e594e93e4d1e
josebasilioo <josebasiliosilvaneto@gmail.com>
2023-12-18 11:11:25 -0300

    main: saving changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React! Outline
+          Hello React! Mudanças
         </p>
         <a
           className="App-link"
